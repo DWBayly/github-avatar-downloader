e8fd0db5b64caa58a7c53eebb144e5fa6328ca28
dbaylyuvic <dbayly@uvic.ca>
2017-11-06 13:13:48 -0800

    step 7

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var token = require('./secret.js');
+var fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -16,12 +17,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
     cb(err, body);
   });
 }
+function downloadImageByURL(url, filePath) {
+  request(url).pipe(fs.createWriteStream(filePath));
+}
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+/*getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   var val = JSON.parse(result);
   console.log(val);
   for(var x of val){
     console.log(x['avatar_url']);
   }
-});
+});*/
