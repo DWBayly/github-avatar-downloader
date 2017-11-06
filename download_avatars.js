e7a45942abef6e50f85ef72aa67682326e3389b1
dbaylyuvic <dbayly@uvic.ca>
2017-11-06 12:44:12 -0800

    modified git ignore to ignore secret.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,21 @@
 var request = require('request');
-
+var token = require('./secret.js');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var r=request( 'https://api.github.com/'+repoOwner+"/"+repoName+'/contributors',cb);  
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization':'token '+token.GITHUB_TOKEN
+      
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
-
-
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
