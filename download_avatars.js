bcf6a62ce2d2dc8c4e391b519fa6d6d4721f3557
dbaylyuvic <dbayly@uvic.ca>
2017-11-06 13:27:18 -0800

    Step 9 and 10

--- download_avatars.js
+++ download_avatars.js
@@ -21,13 +21,19 @@ function downloadImageByURL(url, filePath) {
   request(url).pipe(fs.createWriteStream(filePath));
 }
 /*downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")*/
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  var val = JSON.parse(result);
-  console.log(val);
-  for(var x of val){
-    downloadImageByURL(x['avatar_url'],"avatars/"+x['login']+".jpg")
-    //console.log(x['avatar_url']);
-  }
-});
+var owner = process.argv[2];
+var repo =  process.argv[3];
+//console.log(owner +" "+ repo);
+if(owner=== undefined || repo === undefined){
+  console.log("Invalid values");
+}else{
+    getRepoContributors(owner, repo, function(err, result) {
+    console.log("Errors:", err);
+    var val = JSON.parse(result);
+    console.log(val);
+    for(var x of val){
+      downloadImageByURL(x['avatar_url'],"avatars/"+x['login']+".jpg")
+      //console.log(x['avatar_url']);
+    }
+  });
+}
