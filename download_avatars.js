ddd8db27187847082d24d041626766a4e2720b77
dbaylyuvic <dbayly@uvic.ca>
2017-11-06 12:30:26 -0800

    Step 3

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,14 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var r=request( 'https://api.github.com/'+repoOwner+"/"+repoName+'/contributors',cb);  
+}
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
