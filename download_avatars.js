ff5c0fe38fb278b0bba1e1c336c8955688a578a9
dbaylyuvic <dbayly@uvic.ca>
2017-11-09 23:53:18 -0800

    Fixed all the lint errors

--- download_avatars.js
+++ download_avatars.js
@@ -8,11 +8,9 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization':'token '+token.GITHUB_TOKEN
-      
+      'Authorization': 'token ' + token.GITHUB_TOKEN
     }
   };
-
   request(options, function(err, res, body) {
     cb(err, body);
   });
@@ -20,20 +18,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
 function downloadImageByURL(url, filePath) {
   request(url).pipe(fs.createWriteStream(filePath));
 }
-/*downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")*/
 var owner = process.argv[2];
 var repo =  process.argv[3];
-//console.log(owner +" "+ repo);
-if(owner=== undefined || repo === undefined){
+if(owner === undefined || repo === undefined){
   console.log("Invalid values");
 }else{
-    getRepoContributors(owner, repo, function(err, result) {
+  getRepoContributors(owner, repo, function(err, result) {
     console.log("Errors:", err);
     var val = JSON.parse(result);
     console.log(val);
     for(var x of val){
-      downloadImageByURL(x['avatar_url'],"avatars/"+x['login']+".jpg")
-      //console.log(x['avatar_url']);
+      downloadImageByURL(x['avatar_url'], "avatars/" + x['login'] + ".jpg");
     }
   });
 }
